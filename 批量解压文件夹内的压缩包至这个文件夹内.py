148f014e62023242967f3043fa3e9214cf456498
VIC7894888 <164737095+VIC7894888@users.noreply.github.com>
2024-05-25 14:15:59 +0800

    Add files via upload
    批量解压文件夹内的压缩包至这个文件夹内

---
+++ 批量解压文件夹内的压缩包至这个文件夹内.py
@@ -0,0 +1,16 @@
+import os
+import zipfile
+
+def unzip_all_in_folder(folder_path):
+    for item in os.listdir(folder_path):
+        item_path = os.path.join(folder_path, item)
+        if os.path.isfile(item_path) and item.endswith('.zip'):
+            with zipfile.ZipFile(item_path, 'r') as zip_ref:
+                zip_ref.extractall(folder_path)
+                print(f"解压 '{item}' 完成.")
+        elif os.path.isdir(item_path):
+            unzip_all_in_folder(item_path)
+
+# 使用时指定要解压的文件夹路径
+folder_to_unzip = 'E:\建筑\补充素材\地标'
+unzip_all_in_folder(folder_to_unzip)
